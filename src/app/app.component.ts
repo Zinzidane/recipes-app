df6c4d03909f7b1b8b86da001a3610ce537a1812
ivan muliarchyk <ya-gans@mail.ru>
2017-11-17 23:51:18 +0300

    Add auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBsvy99AH0k9q4tPKJkEeZBKXPyw-GZlpA",
+      authDomain: "recipe-book-a8717.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
+
 }
