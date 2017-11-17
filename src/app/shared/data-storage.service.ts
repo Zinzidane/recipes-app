412ae81cc38cc956a1141e663e26c8c8891d3734
ivan muliarchyk <ya-gans@mail.ru>
2017-11-17 20:37:23 +0300

    Add get request to fetch recipes from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Response } from '@angular/http';
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable()
@@ -8,5 +9,25 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://recipe-book-a8717.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    return this.http.get('https://recipe-book-a8717.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if(!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
