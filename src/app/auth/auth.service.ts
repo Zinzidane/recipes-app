564ebbec705df1766ca67c08295261cea8083b49
ivan muliarchyk <ya-gans@mail.ru>
2017-11-21 21:25:47 +0300

    change routing after signin to recipes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
-          this.router.navigate(['/']);
+          this.router.navigate(['/recipes']);
           firebase.auth().currentUser.getIdToken()
             .then(
               (token: string) => {
