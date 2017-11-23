6e59bb44454a0489ca4a547699b2077e81c0bc5f
ivan muliarchyk <ya-gans@mail.ru>
2017-11-23 21:06:53 +0300

    Refactor recipe.service with shoppinglistactions

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
+import { Store } from '@ngrx/store';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from 'rxjs/Subject';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -10,18 +12,19 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
       new Recipe(
-        'Roasted chicken', 
-        'So delicious', 
+        'Roasted chicken',
+        'So delicious',
         'https://upload.wikimedia.org/wikipedia/commons/1/1b/Roasted_Chicken_Dinner_Plate%2C_Broccoli%2C_Demi_Glace.jpg',
         [new Ingredient('Chicken', 1, 'pieces'), new Ingredient('Broccoli', 4, 'pieces')]),
       new Recipe(
-        'Apple Pie', 
-        'Never never gonna give you up!', 
+        'Apple Pie',
+        'Never never gonna give you up!',
         'https://upload.wikimedia.org/wikipedia/commons/f/f2/Chicken_Pie.JPG',
         [new Ingredient('Apple', 3, 'pieces'), new Ingredient('Eggs', 4, 'pieces'), new Ingredient('Sugar', 1, 'glass')])
   ];
 
-  constructor(private slService: ShoppingListService) {} 
+  constructor(private slService: ShoppingListService, private store: Store<{shoppingList: 
+    {ingredients: Ingredient[]}}>) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -37,7 +40,7 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   addRecipe(recipe: Recipe) {
