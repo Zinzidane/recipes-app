447b324b51667a0f627809b17c8771e2aead1d36
ivan muliarchyk <ya-gans@mail.ru>
2017-11-27 14:57:09 +0300

    Implement side effect to store recipes

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,13 @@
 import { Recipe } from '../recipe.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
 
 import * as RecipeActions from './recipe.actions';
+import * as fromRecipe from './recipe.reducers';
 
 @Injectable()
 export class RecipeEffects {
@@ -30,8 +33,20 @@ export class RecipeEffects {
           payload: recipes
         };
       }
-    );
+  );
+
+  @Effect({dispatch: false})
+  recipeStore = this.actions$
+    .ofType(RecipeActions.STORE_RECIPES)
+    .withLatestFrom(this.store.select('recipes'))
+    .switchMap(([action, state]) => {
+      const req = new HttpRequest('PUT', 'https://recipe-book-a8717.firebaseio.com/recipes.json',
+      state.recipes, {reportProgress: true});
+      return this.httpClient.request(req);
+    });
+  
 
   constructor(private actions$: Actions,
-              private httpClient: HttpClient) {}
+              private httpClient: HttpClient,
+              private store: Store<fromRecipe.FeatureState>) {}
 }
